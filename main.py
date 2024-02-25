981f7c47ecbada165b7b39e3f571ca7a584a39cc
Marco von Rosenberg <codingmarco@gmail.com>
2024-02-25 14:37:02 +0100

    Swap ip and video args, add crop-resizing

--- main.py
+++ main.py
@@ -5,10 +5,30 @@ import time
 from ddp import DDPDevice
 
 
+def resize_keep_aspect_ratio(image, target_width, target_height):
+    # First crop the image to the target aspect ratio
+    aspect_ratio = target_width / target_height
+    image_aspect_ratio = image.shape[1] / image.shape[0]
+
+    if image_aspect_ratio > aspect_ratio:
+        # Crop the width
+        new_width = int(image.shape[0] * aspect_ratio)
+        start = (image.shape[1] - new_width) // 2
+        image = image[:, start : start + new_width]
+    else:
+        # Crop the height
+        new_height = int(image.shape[1] / aspect_ratio)
+        start = (image.shape[0] - new_height) // 2
+        image = image[start : start + new_height, :]
+    # Resize to the target size
+    image = cv2.resize(image, (target_width, target_height))
+    return image
+
+
 def main():
     parser = argparse.ArgumentParser(description="Play video on DDP")
-    parser.add_argument("video", help="Path to video file")
     parser.add_argument("ip", help="IP address of DDP")
+    parser.add_argument("video", help="Path to video file")
     args = parser.parse_args()
 
     vidcap = cv2.VideoCapture(args.video)
@@ -21,7 +41,9 @@ def main():
         if not success:
             break
 
-        ddp.flush(image / 255.0 * 80)
+        image = resize_keep_aspect_ratio(image, 16, 16)
+
+        ddp.flush(image)
         frame_count += 1
 
         # Limit to 25fps
