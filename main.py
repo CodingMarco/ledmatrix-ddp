4c2501758c100383ddf85db7e588d8a7609147bd
Marco von Rosenberg <codingmarco@gmail.com>
2024-02-25 15:13:34 +0100

    Get matrix dimensions from WLED API

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 import cv2
 import time
+import requests
+import socket
 
 from ddp import DDPDevice
 
@@ -25,16 +27,29 @@ def resize_keep_aspect_ratio(image, target_width, target_height):
     return image
 
 
+def get_matrix_dimensions(host):
+    # Get WLED infos
+    response = requests.get(f"http://{host}/json/info")
+    response.raise_for_status()
+    matrix = response.json()["leds"]["matrix"]
+    return matrix["w"], matrix["h"]
+
+
 def main():
     parser = argparse.ArgumentParser(description="Play video on DDP")
     parser.add_argument("ip", help="IP address of DDP")
     parser.add_argument("video", help="Path to video file")
     args = parser.parse_args()
 
+    # This seems to be much much faster than
+    # letting `requests` do the DNS resolution
+    ip = socket.gethostbyname(args.ip)
+
     video = cv2.VideoCapture(args.video)
     fps = video.get(cv2.CAP_PROP_FPS)
     frame_delay = 1.0 / fps
-    ddp = DDPDevice(args.ip)
+    ddp = DDPDevice(ip)
+    matrix_w, matrix_h = get_matrix_dimensions(ip)
 
     frame_count = 0
     last_frame = time.time()
@@ -43,8 +58,9 @@ def main():
         if not success:
             break
 
-        image = resize_keep_aspect_ratio(image, 16, 16)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = resize_keep_aspect_ratio(image, matrix_w, matrix_h)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) - 100.0
+        image = image.clip(0, 255)
 
         ddp.flush(image)
         frame_count += 1
